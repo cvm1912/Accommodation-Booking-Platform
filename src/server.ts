5bafb8a73e0724dd901f97e4b82a02f0c86cf9df
cvm1912 <rajshivam1912@gmail.com>
2026-09-13 17:55:07 +0530

    feat(starter-project): express typescript starter porject

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import {Express} from "express";
+
+
+const app : Express= express();
+const PORT : Number  = 8080;
+
+app.get('/ping',(req,res) => res.send('pong'));
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
