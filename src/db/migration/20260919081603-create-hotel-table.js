3e6dc4b2e9680c164f690870ff4b8c1c69d33be3
cvm1912 <rajshivam1912@gmail.com>
2026-09-19 14:11:13 +0530

    added

---
+++ src/db/migration/20260919081603-create-hotel-table.js
@@ -0,0 +1,53 @@
+
+
+module.exports = {
+    async up(queryInterface) {
+       // await queryInterface.createTable("hotels", {
+    //   id: {
+    //     type: "INTEGER",
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     allowNull: false,
+    //   },
+    //   name: {
+    //     type: "VARCHAR(255)",
+    //     allowNull: false,
+    //   },
+    //   location: {
+    //     type: "VARCHAR(255)",
+    //     allowNull: false,
+    //   },
+    //   rating: {
+    //     type: "FLOAT",
+    //     allowNull: false,
+    //   },
+    //   createdAt: {
+    //     type: "TIMESTAMP",
+    //     defaultValue: new Date(),
+    //     allowNull: false,
+    //   },
+    //   updatedAt: {
+    //     type: "TIMESTAMP",
+    //     defaultValue: new Date(),
+    //     allowNull: false,
+    //   },
+    // });
+ 
+        await queryInterface.sequelize.query(`
+            CREATE TABLE IF NOT EXISTS hotels (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
+    },
+
+    async down(queryInterface) {
+        await queryInterface.sequelize.query(`
+            DROP TABLE IF EXISTS hotels;
+        `);
+    }
+};
