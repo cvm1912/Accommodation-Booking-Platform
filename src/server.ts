9cf685df26e38d268d86e5ca5cf9f31489429d5b
cvm1912 <rajshivam1912@gmail.com>
2026-09-19 15:58:02 +0530

    added table and model

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import { Express } from 'express';
+import express, { Express } from 'express';
 import logger from './config/logger';
-import { loadEnv, serverConfig } from './config';
+import { serverConfig } from './config';
 import v1Router from './routes/v1';
 import { errorMiddleware } from './middlewares/error-middleware';
-import {attachCorrelationalId} from './middlewares/corelation-middleware'
-
+import { attachCorrelationalId } from './middlewares/corelation-middleware';
+import sequelize from './db/models/sequelize';
+import Hotel from './db/models/hotel';
 
 const app: Express = express();
 
@@ -15,7 +15,22 @@ app.use(attachCorrelationalId);
 app.use('/api/v1', v1Router);
 app.use(errorMiddleware);
 
-app.listen(serverConfig.PORT, () => logger.info(`Server is listening on ${serverConfig.PORT}`));
-
-
-
+app.listen(serverConfig.PORT, async () => {
+    logger.info(`Server is listening on ${serverConfig.PORT}`);
+    try {
+        await sequelize.authenticate();
+        logger.info('DB Connected');
+        const hotel = await Hotel.create({
+            name: 'test',
+            address: 'test desc',
+            location: 'test loc',
+            rating: 5,
+            rating_count: 0,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+        logger.info(`Hotel created: ${JSON.stringify(hotel)}`);
+    } catch (e) {
+        logger.error(e);
+    }
+});
